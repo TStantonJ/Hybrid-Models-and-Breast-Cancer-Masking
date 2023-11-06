f2082eaed7b5cc8ef838089613820efaca624c72
ThomasRStanton <thomasrstantonjr@gmail.com>
2023-11-05 19:50:31 -0500

    Linear Regression

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from Utility.ProgressBar import progressBar
 from Models.KNN import KNNTrain
 from Models.RBFSVR import SVMRBFTrain
+from Models.LinearRegression import LinearTrain
 from Models.RandomForest import RadomForestTrain
 from Models.GradientBoost import GradientBoostTrain
 from Models.AdaBoost import AdaBoostTrain
@@ -29,27 +30,31 @@ data_object = {'X_train':X_train, 'X_test':X_test, 'y_train':y_train, 'y_test':y
 
 # Run models
 results = []
-progressBar(0,5, ' KNN Training')
+progressBar(0,6, ' KNN Training')
 KNN_result = KNNTrain(**data_object)
 results.append(['KNN', KNN_result[0], KNN_result[1]])
 
-progressBar(1,5, ' SVM RBF Training')
+progressBar(1,6, ' SVM RBF Training')
 SVMRBF_result = SVMRBFTrain(**data_object)
 results.append(['SVM RBF', SVMRBF_result[0], SVMRBF_result[1]])
 
-progressBar(2,5, ' Random Forest Training')
+progressBar(2,6, ' Linear Regression Training')
+Linear_result = LinearTrain(**data_object)
+results.append(['Linear Regression', Linear_result[0], Linear_result[1]])
+
+progressBar(3,6, ' Random Forest Training')
 RadomForest_result = RadomForestTrain(**data_object)
 results.append(['Radom Forest', RadomForest_result[0], RadomForest_result[1]])
 
-progressBar(3,5, ' Gradient Boost Training')
+progressBar(4,6, ' Gradient Boost Training')
 GradientBoost_result = GradientBoostTrain(**data_object)
 results.append(['Gradient Boost', GradientBoost_result[0], GradientBoost_result[1]])
 
-progressBar(4,5, ' Ada Boost Training')
+progressBar(5,6, ' Ada Boost Training')
 AdaBoost_result = AdaBoostTrain(**data_object)
 results.append(['Ada Boost', AdaBoost_result[0], AdaBoost_result[1]])
 
-progressBar(5,5)
+progressBar(6,6)
 
 # Format Results 
 for i in results:
